4e9e1eaf7a8b73ff1bfcae93951041ed4fd5eacb
dxvgef <dxvgef@outlook.com>
2022-08-09 18:33:40 +0800

    - 移除 lestrrat-go/jwx - 调整代码结构

--- jwt_test.go
+++ jwt_test.go
@@ -2,51 +2,24 @@ package main
 
 import (
 	"crypto/rsa"
-	"encoding/json"
 	"errors"
-	"io/ioutil"
 	"log"
 	"os"
 	"testing"
 	"time"
 
-	cristalhq "github.com/cristalhq/jwt/v3"
+	cristalhq "github.com/cristalhq/jwt/v4"
 	dgrijalva "github.com/dgrijalva/jwt-go"
 	gbrlsnchs "github.com/gbrlsnchs/jwt/v3"
-	"github.com/lestrrat-go/jwx/jwa"
-	lestrrat "github.com/lestrrat-go/jwx/jwt"
 	pascaldekloe "github.com/pascaldekloe/jwt"
 )
 
 var (
-	publicKey    *rsa.PublicKey
-	privateKey   *rsa.PrivateKey
-	tokenStr     string
-	gbrlsnchsAlg gbrlsnchs.Algorithm
-
-	dgrijalvaToken    DgrijalvaToken
-	pascaldekloeToken PascaldekloeToken
-	gbrlsnchsToken    GbrlsnchsToken
-	cristalhqToken    CristalhqToken
-	lestrratToken     = lestrrat.New()
-	cristalhqSigner   cristalhq.Signer
+	publicKey  *rsa.PublicKey
+	privateKey *rsa.PrivateKey
+	tokenStr   string
+	tokenBytes []byte
 )
-
-type DgrijalvaToken struct {
-	Data struct {
-		ID       string `json:"id,omitempty"`
-		Username string `json:"username,omitempty"`
-	} `json:"data,omitempty"`
-	dgrijalva.StandardClaims
-}
-
-type PascaldekloeToken struct {
-	Data struct {
-		ID       string `json:"id,omitempty"`
-		Username string `json:"username,omitempty"`
-	} `json:"data,omitempty"`
-	pascaldekloe.Claims
-}
 
 type GbrlsnchsToken struct {
 	Data struct {
@@ -54,18 +27,6 @@ type GbrlsnchsToken struct {
 		Username string `json:"username,omitempty"`
 	} `json:"data,omitempty"`
 	gbrlsnchs.Payload
-}
-
-type CristalhqToken struct {
-	Data struct {
-		ID       string `json:"id,omitempty"`
-		Username string `json:"username,omitempty"`
-	} `json:"data,omitempty"`
-	cristalhq.StandardClaims
-}
-
-func (t *CristalhqToken) MarshalBinary() ([]byte, error) {
-	return json.Marshal(t)
 }
 
 func keyFunc(token *dgrijalva.Token) (interface{}, error) {
@@ -77,91 +38,131 @@ func keyFunc(token *dgrijalva.Token) (interface{}, error) {
 
 func init() {
 	log.SetFlags(log.Lshortfile)
-	err := loadKey()
-	if err != nil {
-		log.Println(err.Error())
-		os.Exit(1)
-	}
 
-	dgrijalvaToken.Data.ID = "12345"
-	dgrijalvaToken.Data.Username = "dxvgef"
-
-	pascaldekloeToken.Data.ID = "12345"
-	pascaldekloeToken.Data.Username = "dxvgef"
-
-	gbrlsnchsToken.Data.ID = "12345"
-	gbrlsnchsToken.Data.Username = "dxvgef"
-
-	cristalhqToken.Data.ID = "12345"
-	cristalhqToken.Data.Username = "dxvgef"
-
-	cristalhqSigner, err = cristalhq.NewSignerRS(cristalhq.RS256, privateKey)
-	if err != nil {
-		log.Fatalln(err.Error())
-	}
-
-	if err = lestrratToken.Set("id", "12345"); err != nil {
-		log.Fatalln(err.Error())
-	}
-	if err = lestrratToken.Set("username", "dxvgef"); err != nil {
-		log.Fatalln(err.Error())
-	}
-}
-
-func loadKey() error {
 	// 读取公钥
-	publicKeyByte, err := ioutil.ReadFile("./public.key")
+	publicKeyByte, err := os.ReadFile("./public.key")
 	if err != nil {
 		log.Println(err.Error())
-		return err
+		return
 	}
 	publicKey, err = dgrijalva.ParseRSAPublicKeyFromPEM(publicKeyByte)
 	if err != nil {
 		log.Println(err.Error())
-		return err
+		return
 	}
 
 	// 读取私钥
-	privateKeyByte, err := ioutil.ReadFile("./private.key")
+	privateKeyByte, err := os.ReadFile("./private.key")
 	if err != nil {
 		log.Println(err.Error())
-		return err
+		return
 	}
 	privateKey, err = dgrijalva.ParseRSAPrivateKeyFromPEM(privateKeyByte)
 	if err != nil {
 		log.Println(err.Error())
-		return err
+		return
 	}
-
-	gbrlsnchsAlg = gbrlsnchs.NewRS256(
-		gbrlsnchs.RSAPublicKey(publicKey),
-		gbrlsnchs.RSAPrivateKey(privateKey),
-	)
-	return nil
 }
 
-func Benchmark_dgrijalva_sign(b *testing.B) {
-	var err error
+// --------------------------- begin cristalhq -----------------------------------
+func Benchmark_cristalhq_sign(b *testing.B) {
+	type Claims struct {
+		Data struct {
+			ID       string `json:"id,omitempty"`
+			Username string `json:"username,omitempty"`
+		} `json:"data,omitempty"`
+		cristalhq.RegisteredClaims
+	}
+
+	var (
+		err    error
+		claims Claims
+		signer *cristalhq.RSAlg
+		token  *cristalhq.Token
+	)
+
+	claims.Data.ID = "12345"
+	claims.Data.Username = "dxvgef"
+	claims.ExpiresAt = cristalhq.NewNumericDate(time.Now().Add(3 * time.Second))
+
+	signer, err = cristalhq.NewSignerRS(cristalhq.RS256, privateKey)
+	if err != nil {
+		b.Errorf(err.Error())
+		return
+	}
+
 	for i := 0; i < b.N; i++ {
-		dgrijalvaToken.ExpiresAt = time.Now().Add(3 * time.Second).Unix()
-		token := dgrijalva.NewWithClaims(dgrijalva.SigningMethodRS256, dgrijalvaToken)
+		token, err = cristalhq.NewBuilder(signer).Build(claims)
+		if err != nil {
+			b.Error(err)
+			return
+		}
+	}
+	tokenStr = bytesToStr(token.Bytes())
+	tokenBytes = token.Bytes()
+}
+
+func Benchmark_cristalhq_verify(b *testing.B) {
+	verifier, err := cristalhq.NewVerifierRS(cristalhq.RS256, publicKey)
+	if err != nil {
+		b.Error(err)
+		return
+	}
+	for i := 0; i < b.N; i++ {
+		_, err = cristalhq.Parse(tokenBytes, verifier)
+		if err != nil {
+			b.Error(err)
+			return
+		}
+	}
+}
+
+// --------------------------- end cristalhq -----------------------------------
+
+// --------------------------- begin dgrijalva ----------------------------------
+func Benchmark_dgrijalva_sign(b *testing.B) {
+	type Claims struct {
+		Data struct {
+			ID       string `json:"id,omitempty"`
+			Username string `json:"username,omitempty"`
+		} `json:"data,omitempty"`
+		dgrijalva.StandardClaims
+	}
+	var (
+		err    error
+		claims Claims
+		token  *dgrijalva.Token
+	)
+	claims.Data.ID = "12345"
+	claims.Data.Username = "dxvgef"
+	claims.ExpiresAt = time.Now().Add(3 * time.Second).Unix()
+	token = dgrijalva.NewWithClaims(dgrijalva.SigningMethodRS256, claims)
+
+	for i := 0; i < b.N; i++ {
 		tokenStr, err = token.SignedString(privateKey)
 		if err != nil {
 			b.Error(err)
 			return
 		}
 	}
+
+	tokenBytes = strToBytes(tokenStr)
 }
 
 func Benchmark_dgrijalva_verify(b *testing.B) {
+	type Claims struct {
+		Data struct {
+			ID       string `json:"id,omitempty"`
+			Username string `json:"username,omitempty"`
+		} `json:"data,omitempty"`
+		dgrijalva.StandardClaims
+	}
 	for i := 0; i < b.N; i++ {
-		// 用公钥验证Token合法性，并解析出一个token对象的指针
-		token, err := dgrijalva.ParseWithClaims(tokenStr, &DgrijalvaToken{}, keyFunc)
+		token, err := dgrijalva.ParseWithClaims(tokenStr, &Claims{}, keyFunc)
 		if err != nil {
 			b.Error(err)
 			return
 		}
-
 		// 验证token是否有效
 		err = token.Claims.Valid()
 		if err != nil {
@@ -171,20 +172,37 @@ func Benchmark_dgrijalva_verify(b *testing.B) {
 	}
 }
 
+// --------------------------- end dgrijalva ----------------------------------
+
+// --------------------------- begin pascaldekloe -----------------------------------
 func Benchmark_pascaldekloe_sign(b *testing.B) {
+	type Token struct {
+		Data struct {
+			ID       string `json:"id,omitempty"`
+			Username string `json:"username,omitempty"`
+		} `json:"data,omitempty"`
+		pascaldekloe.Claims
+	}
+
+	var (
+		err   error
+		token Token
+	)
+	token.Data.ID = "12345"
+	token.Data.Username = "dxvgef"
+	token.Expires = pascaldekloe.NewNumericTime(time.Now().Add(3 * time.Second))
+
 	for i := 0; i < b.N; i++ {
-		pascaldekloeToken.Expires = pascaldekloe.NewNumericTime(time.Now().Add(3 * time.Second))
-		tokenBytes, err := pascaldekloeToken.RSASign(pascaldekloe.RS256, privateKey)
+		tokenBytes, err = token.RSASign(pascaldekloe.RS256, privateKey)
 		if err != nil {
 			b.Error(err)
 			return
 		}
-		tokenStr = bytesToStr(tokenBytes)
 	}
+	tokenStr = bytesToStr(tokenBytes)
 }
 
 func Benchmark_pascaldekloe_verify(b *testing.B) {
-	tokenBytes := strToBytes(tokenStr)
 	for i := 0; i < b.N; i++ {
 		_, err := pascaldekloe.RSACheck(tokenBytes, publicKey)
 		if err != nil {
@@ -194,29 +212,40 @@ func Benchmark_pascaldekloe_verify(b *testing.B) {
 	}
 }
 
-func Benchmark_gbrlsnchs_sign(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		gbrlsnchsToken.ExpirationTime = gbrlsnchs.NumericDate(time.Now().Add(3 * time.Second))
-		tokenBytes, err := gbrlsnchs.Sign(gbrlsnchsToken, gbrlsnchsAlg)
-		if err != nil {
-			b.Error(err)
-			return
-		}
-		tokenStr = bytesToStr(tokenBytes)
+// --------------------------- end pascaldekloe -----------------------------------
 
-		var token GbrlsnchsToken
-		_, err = gbrlsnchs.Verify(tokenBytes, gbrlsnchsAlg, &token)
+// ----------------------------------- begin gbrlsnchs ------------------------------
+func Benchmark_gbrlsnchs_sign(b *testing.B) {
+	var (
+		err   error
+		token GbrlsnchsToken
+	)
+	token.Data.ID = "12345"
+	token.Data.Username = "dxvgef"
+	token.ExpirationTime = gbrlsnchs.NumericDate(time.Now().Add(3 * time.Second))
+
+	gbrlsnchsAlg := gbrlsnchs.NewRS256(
+		gbrlsnchs.RSAPublicKey(publicKey),
+		gbrlsnchs.RSAPrivateKey(privateKey),
+	)
+
+	for i := 0; i < b.N; i++ {
+		tokenBytes, err = gbrlsnchs.Sign(token, gbrlsnchsAlg)
 		if err != nil {
 			b.Error(err)
 			return
 		}
 	}
+	tokenStr = bytesToStr(tokenBytes)
 }
 
 func Benchmark_gbrlsnchs_verify(b *testing.B) {
-	tokenBytes := strToBytes(tokenStr)
+	var token GbrlsnchsToken
+	gbrlsnchsAlg := gbrlsnchs.NewRS256(
+		gbrlsnchs.RSAPublicKey(publicKey),
+		gbrlsnchs.RSAPrivateKey(privateKey),
+	)
 	for i := 0; i < b.N; i++ {
-		var token GbrlsnchsToken
 		_, err := gbrlsnchs.Verify(tokenBytes, gbrlsnchsAlg, &token)
 		if err != nil {
 			b.Error(err)
@@ -225,57 +254,4 @@ func Benchmark_gbrlsnchs_verify(b *testing.B) {
 	}
 }
 
-func Benchmark_cristalhq_sign(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		cristalhqToken.ExpiresAt = cristalhq.NewNumericDate(time.Now().Add(3 * time.Second))
-		tokenBytes, err := cristalhq.BuildBytes(cristalhqSigner, &cristalhqToken)
-		if err != nil {
-			b.Error(err)
-			return
-		}
-		tokenStr = bytesToStr(tokenBytes)
-	}
-}
-
-func Benchmark_cristalhq_verify(b *testing.B) {
-	verifier, err := cristalhq.NewVerifierRS(cristalhq.RS256, publicKey)
-	if err != nil {
-		b.Error(err)
-		return
-	}
-	tokenBytes := strToBytes(tokenStr)
-	for i := 0; i < b.N; i++ {
-		_, err = cristalhq.ParseAndVerify(tokenBytes, verifier)
-		if err != nil {
-			b.Error(err)
-			return
-		}
-	}
-}
-
-func Benchmark_lestrrat_sign(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		err := lestrratToken.Set(lestrrat.ExpirationKey, time.Now().Add(3*time.Second).Unix())
-		if err != nil {
-			b.Error(err.Error())
-			return
-		}
-		tokenBytes, err := lestrrat.Sign(lestrratToken, jwa.RS256, privateKey)
-		if err != nil {
-			b.Error(err)
-			return
-		}
-		tokenStr = bytesToStr(tokenBytes)
-	}
-}
-
-func Benchmark_lestrrat_verify(b *testing.B) {
-	tokenBytes := strToBytes(tokenStr)
-	for i := 0; i < b.N; i++ {
-		_, err := lestrrat.Parse(tokenBytes, lestrrat.WithValidate(true))
-		if err != nil {
-			b.Error(err)
-			return
-		}
-	}
-}
+// ----------------------------------- end gbrlsnchs ------------------------------
